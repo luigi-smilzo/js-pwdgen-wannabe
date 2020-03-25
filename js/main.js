126ffe7ee3a240bc52cd3bb9ab84f5bf63017bd8
Luigi <luigismilzo@live.com>
2020-03-25 17:23:12 +0100

    Password Generator

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var firstName = prompt('Inserisci il tuo nome');
+
+var lastName = prompt('Inserisci il tuo congnome');
+
+var color = prompt('Qual è il tuo colore preferito?');
+
+console.log('Nome:', firstName, 'Cognome:', lastName,   'Colore preferito:', color);
+
+var password = firstName + lastName + color;
+
+console.log(password);
+
+document.getElementById('psw').innerHTML = firstName + lastName + color + '20';
